532d4e85de78f324b64e0b7844d84b3c163106f5
Cupi <cupi@airdroptour.com>
2026-08-05 01:39:54 +0300

    fix: remove stray character on import line

--- lib/page/channels_page.dart
+++ lib/page/channels_page.dart
@@ -1,4 +1,4 @@
-cimport 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:airdrop/services/bybugdb_bridge.dart';
 import 'package:airdrop/theme/color.dart';
 import 'package:airdrop/widget/text.dart';
