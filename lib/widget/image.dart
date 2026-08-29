1f3e9e575dd98cf65dc50e0d13e7b04ac036b142
Adın Soyadın <github-mailin@ornek.com>
2026-08-29 19:34:19 +0300

    fix: parseURL artik URL'i degistirmiyor - tum resimler backendden geldigi gibi kullaniliyor

--- lib/widget/image.dart
+++ lib/widget/image.dart
@@ -7,9 +7,7 @@ import 'dart:typed_data';
 import 'package:http/http.dart' as http;
 
 String parseURL(String url) {
-  if (url.isEmpty) return url;
-  String fileName = url.split("/")[url.split("/").length - 1];
-  return "https://appairdroptour.yurtdisiisilanlari.com.tr/publicFile/$fileName";
+  return url;
 }
 
 class ImageNetworks extends StatefulWidget {
